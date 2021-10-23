f5a954bf10145a0df5217a651f458d2f37ce83c2
StanleyPangaruy <pangaruystanley@gmail.com>
2021-10-23 23:50:56 +0800

    A program that asks for certain details that will be displayed at end.

---
+++ program1.py
@@ -0,0 +1,12 @@
+"""
+A simple program that asks for name, age and address.
+The details will be displayed in a certain format.
+"""
+
+#These variables are for the needed details
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+address = input('Enter your address: ')
+
+#This statement displays the details entered following a certain format.
+print(f'Hi, my name is {name}. I am {age} years old and I live in {address}.')
